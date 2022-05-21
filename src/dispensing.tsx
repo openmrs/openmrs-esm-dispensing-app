c4a75fcac6082f1f0bfc7a4040b40a29bdf8924b
Brandon Istenes <bistenes@gmail.com>
2022-05-20 17:18:43 -0700

    Cleanup

--- src/dispensing.tsx
+++ src/dispensing.tsx
@@ -1,13 +1,3 @@
-/**
- * From here, the application is pretty typical React, but with lots of
- * support from `@openmrs/esm-framework`. Check out `Greeter` to see
- * usage of the configuration system, and check out `PatientGetter` to
- * see data fetching using the OpenMRS FHIR API.
- *
- * Check out the Config docs:
- *   https://openmrs.github.io/openmrs-esm-core/#/main/config
- */
-
 import {
   DataTableSkeleton,
   Table,
