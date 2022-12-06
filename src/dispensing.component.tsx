274a58c30cd3f100fe73f79fee73beb8d47e7ff0
pirupius <piruville@gmail.com>
2022-12-06 18:53:30 +0300

    Refactors dispensing dashboard to use extensions for reuse

--- src/dispensing.component.tsx
+++ src/dispensing.component.tsx
@@ -1,16 +1,6 @@
 import React from "react";
-import { PharmacyHeader } from "./pharmacy-header/pharmacy-header.component";
-import DispensingTiles from "./dispensing-tiles/dispensing-tiles.component";
-import PrescriptionTabLists from "./prescriptions/prescription-tab-lists.component";
-import Overlay from "./forms/overlay/overlay.component";
+import { ExtensionSlot } from "@openmrs/esm-framework";
 
 export default function Dispensing() {
-  return (
-    <div className={`omrs-main-content`}>
-      <PharmacyHeader />
-      {/*      <DispensingTiles />*/}
-      <PrescriptionTabLists />
-      <Overlay />
-    </div>
-  );
+  return <ExtensionSlot name="dispensing-dashboard-slot" />;
 }
