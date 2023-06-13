e3e971ce2acd0f2b3cd4e7c854347252a16f7619
Dennis Kigen <kigen.work@gmail.com>
2023-06-13 23:12:28 +0300

    (BREAKING) Migrate `esm-dispensing` to use routes.json

--- src/index.ts
+++ src/index.ts
@@ -1,63 +1,35 @@
 import { getAsyncLifecycle, defineConfigSchema } from "@openmrs/esm-framework";
 import { configSchema } from "./config-schema";
 
-declare var __VERSION__: string;
-const version = __VERSION__;
-
-const importTranslation = require.context(
+export const importTranslation = require.context(
   "../translations",
   false,
   /.json$/,
   "lazy"
 );
 
-const backendDependencies = {
-  fhir2: "^1.2.0",
-  "webservices.rest": "^2.2.0",
+const moduleName = "@openmrs/esm-dispensing-app";
+
+const options = {
+  featureName: "dispensing",
+  moduleName,
 };
 
-function setupOpenMRS() {
-  const moduleName = "@openmrs/esm-dispensing-app";
+export const dispensing = getAsyncLifecycle(
+  () => import("./dispensing.component"),
+  options
+);
 
-  const options = {
-    featureName: "dispensing",
-    moduleName,
-  };
+export const dispensingLink = getAsyncLifecycle(
+  () => import("./dispensing-link"),
+  options
+);
 
+export const dispensingDashboard = getAsyncLifecycle(
+  () => import("./dashboard/dispensing-dashboard.component"),
+  options
+);
+
+export function startupApp() {
   defineConfigSchema(moduleName, configSchema);
-
-  return {
-    pages: [
-      {
-        route: "dispensing",
-        load: getAsyncLifecycle(
-          () => import("./dispensing.component"),
-          options
-        ),
-        online: true,
-        offline: true,
-      },
-    ],
-    extensions: [
-      {
-        id: "dispensing-link",
-        slot: "app-menu-slot",
-        load: getAsyncLifecycle(() => import("./dispensing-link"), options),
-        online: true,
-        offline: false,
-      },
-      {
-        name: "dispensing-dashboard",
-        slot: "dispensing-dashboard-slot",
-        load: getAsyncLifecycle(
-          () => import("./dashboard/dispensing-dashboard.component"),
-          options
-        ),
-        online: true,
-        offline: true,
-      },
-    ],
-  };
 }
-
-export { backendDependencies, importTranslation, setupOpenMRS, version };
