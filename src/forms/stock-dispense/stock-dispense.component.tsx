2cbd523f58c64369b7e37bda3715638d03fce379
Felix Kiprotich <72152368+FelixKiprotich350@users.noreply.github.com>
2024-07-18 22:31:11 +0300

    (fix) Fix reverseal of autochange quantity commit and removed unused code (#112)
    * Improved Dispensing module- Selectable items must have quantities greater or equal to the dispensing quantity
    
    * camelcase naming fix
    
    * fixed count condition to be greater than 0 only
    
    * enabled autopick batch quantity in cases where it is less than prescribed quantity
    
    * Corrected and simplified date calculation
    
    * Fixed updatequantity when stock is diabled in dispensing module
    
    * (fix) O3-3557: Ensure selectable batches have sufficient quantities for dispensing
    
    * Added a comment for the isValiBatch function which checks the expiry of the drugs
    
    * reverted back quantity dispense component
    
    * removed unused functions in the  Fix the auto change of dispensing quantity when a batch is selected #110 PR
    
    ---------
    
    Co-authored-by: Mark Goodrich <mgoodrich@pih.org>

--- src/forms/stock-dispense/stock-dispense.component.tsx
+++ src/forms/stock-dispense/stock-dispense.component.tsx
@@ -9,14 +9,9 @@ type StockDispenseProps = {
   medicationDispense: MedicationDispense;
   updateInventoryItem: (inventoryItem: InventoryItem) => void;
   inventoryItem: InventoryItem;
-  updateAvailableQuantityToMedicationDispenseQuantity: (inventoryItem: InventoryItem) => void;
 };
 
-const StockDispense: React.FC<StockDispenseProps> = ({
-  medicationDispense,
-  updateInventoryItem,
-  updateAvailableQuantityToMedicationDispenseQuantity,
-}) => {
+const StockDispense: React.FC<StockDispenseProps> = ({ medicationDispense, updateInventoryItem }) => {
   const { t } = useTranslation();
   const drugUuid = medicationDispense?.medicationReference?.reference?.split('/')[1];
   const { inventoryItems, error, isLoading } = useDispenseStock(drugUuid);
