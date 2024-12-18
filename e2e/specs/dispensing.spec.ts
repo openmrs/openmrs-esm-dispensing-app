a5b4e0f5d63899843720de7fe7eaee5f58ce3704
mutajonathan <mutajonathan4@gmail.com>
2024-12-18 11:22:35 +0300

    Refactored boiler plate code to actual test of the dispensing page

---
+++ e2e/specs/dispensing.spec.ts
@@ -0,0 +1,20 @@
+import { test, expect } from '@playwright/test';
+import { DispensingPage } from '../pages';
+
+// This test is a sample E2E test. You can delete it.
+
+test('Navigate to the dispensing page', async ({ page }) => {
+  const dispensingPage = new DispensingPage(page);
+
+  await test.step('When I visit the dispensing page', async () => {
+    await dispensingPage.goto();
+  });
+
+  await test.step('Then I should see the active prescriptions tab', async () => {
+    await expect(
+      dispensingPage.page.getByRole('tab', {
+        name: /active prescriptions/i,
+      }),
+    ).toBeVisible();
+  });
+});
