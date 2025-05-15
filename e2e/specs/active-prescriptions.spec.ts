2cf4c2b11ac0b72bbc64986a4dd3305363a131ea
gitcliff <gitacliff48@gmail.com>
2025-05-15 21:19:21 +0300

    add readme file

--- e2e/specs/active-prescriptions.spec.ts
+++ e2e/specs/active-prescriptions.spec.ts
@@ -1,28 +1,18 @@
 import { expect } from '@playwright/test';
-import { generateRandomPatient, deletePatient, type Patient } from '../commands';
+import { type Patient } from '../commands';
 import { test } from '../core';
 import { DispensingPage } from '../pages';
 
 let patient: Patient;
 
-test.beforeEach(async ({ api }) => {
-  patient = await generateRandomPatient(api);
-});
-
 test('View active prescriptions', async ({ page }) => {
   const dispensingPage = new DispensingPage(page);
 
-  await test.step('When I visit the Dispensing page under the active prescriptions tab', async () => {
+  await test.step('Should navigate to the dispensing page diplaying active prescriptions', async () => {
     await dispensingPage.goTo();
 
-    const activeTab = page.getByRole('tab', { name: 'Active prescriptions' });
-    await expect(activeTab).toContainText('Active prescriptions');
-
-    // const allTab = page.getByRole('tab', { name: 'All prescriptions' });
-    // await expect(allTab).toHaveAttribute('aria-selected', 'false');
+    await test.step('Then I should be at the prescriptions page', async () => {
+      await expect(page).toHaveURL(`${process.env.E2E_BASE_URL}spa/dispensing`);
+    });
   });
-});
-
-test.afterEach(async ({ api }) => {
-  await deletePatient(api, patient.uuid);
 });
