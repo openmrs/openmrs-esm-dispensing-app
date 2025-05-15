125b7713782a32e697eaae0747617c32e026e8d4
gitcliff <gitacliff48@gmail.com>
2025-05-15 20:41:07 +0300

    doing initial e2e setup

--- e2e/specs/active-prescriptions.spec.ts
+++ e2e/specs/active-prescriptions.spec.ts
@@ -1,4 +1,4 @@
-//  import { expect } from '@playwright/test';
+import { expect } from '@playwright/test';
 import { generateRandomPatient, deletePatient, type Patient } from '../commands';
 import { test } from '../core';
 import { DispensingPage } from '../pages';
@@ -14,6 +14,12 @@ test('View active prescriptions', async ({ page }) => {
 
   await test.step('When I visit the Dispensing page under the active prescriptions tab', async () => {
     await dispensingPage.goTo();
+
+    const activeTab = page.getByRole('tab', { name: 'Active prescriptions' });
+    await expect(activeTab).toContainText('Active prescriptions');
+
+    // const allTab = page.getByRole('tab', { name: 'All prescriptions' });
+    // await expect(allTab).toHaveAttribute('aria-selected', 'false');
   });
 });
 
