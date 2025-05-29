e983eb86163d05b0d89ab6ac73bfb61f07c86fad
chibongho <cbho@pih.org>
2025-05-29 15:15:18 -0400

    (fix) O3-4761 Allow dashboard extension to work in other apps (#212)

--- src/dashboard/dispensing-dashboard.component.tsx
+++ src/dashboard/dispensing-dashboard.component.tsx
@@ -1,18 +1,12 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { InlineNotification } from '@carbon/react';
-import { setLeftNav, unsetLeftNav, useConfig, WorkspaceContainer } from '@openmrs/esm-framework';
+import { useConfig } from '@openmrs/esm-framework';
 import { type PharmacyConfig } from '../config-schema';
 import { PharmacyHeader } from '../pharmacy-header/pharmacy-header.component';
 import PrescriptionTabLists from '../prescriptions/prescription-tab-lists.component';
 
 export default function DispensingDashboard() {
-  useEffect(() => {
-    const basePath = window.spaBase + '/dispensing';
-    setLeftNav({ name: 'homepage-dashboard-slot', basePath, mode: 'collapsed' });
-    return () => unsetLeftNav('homepage-dashboard-slot');
-  }, []);
-
   const config = useConfig<PharmacyConfig>();
   const { t } = useTranslation();
   if (config.dispenseBehavior.restrictTotalQuantityDispensed && config.dispenseBehavior.allowModifyingPrescription) {
@@ -33,7 +27,6 @@ export default function DispensingDashboard() {
         <PharmacyHeader />
         {/* <DispensingTiles /> */}
         <PrescriptionTabLists />
-        <WorkspaceContainer key="dispensing" contextKey="dispensing" />
       </div>
     );
   }
