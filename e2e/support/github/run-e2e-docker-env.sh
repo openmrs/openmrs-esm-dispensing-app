32515094cd1e5d9f9f4ac3cde2b830eca5386537
Dennis Kigen <kigen.work@gmail.com>
2025-09-30 11:50:19 +0300

    Make yet more tweaks

--- e2e/support/github/run-e2e-docker-env.sh
+++ e2e/support/github/run-e2e-docker-env.sh
@@ -4,45 +4,38 @@
 script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 # create a temporary working directory
 working_dir=$(mktemp -d "${TMPDIR:-/tmp/}openmrs-e2e-frontends.XXXXXXXXXX")
-# get a list of all the apps in this workspace
-apps=$(yarn workspaces list --json | jq -r 'select(.name | test("-app")) | .name')
-# this array will hold all of the packed app names
-app_names=()
+# get the app name
+app_name=$(jq -r '.name' "$script_dir/../../../package.json")
 
-echo "Packing local dispensing app..."
-yarn workspace "@openmrs/esm-dispensing-app" pack -o "$working_dir/_openmrs_esm_dispensing_app.tgz" >/dev/null
-app_names+=("_openmrs_esm_dispensing_app.tgz")
+echo "Creating packed archive of the app..."
+# @openmrs/esm-whatever -> _openmrs_esm_whatever
+packed_app_name=$(echo "$app_name" | tr '[:punct:]' '_');
+# run yarn pack for our app and add it to the working directory
+yarn pack -o "$working_dir/$packed_app_name.tgz" >/dev/null;
+echo "Created packed app archives"
 
-echo "Packing other apps..."
-for app in $apps; do
-  if [ "$app" != "@openmrs/esm-dispensing-app" ]; then
-    app_name=$(echo "$app" | tr '[:punct:]' '_')
-    app_names+=("$app_name.tgz")
-    yarn workspace "$app" pack -o "$working_dir/$app_name.tgz" >/dev/null
-  fi
-done
-
-echo "Generating spa-assemble-config.json..."
+echo "Creating dynamic spa-assemble-config.json..."
+# dynamically assemble our list of frontend modules, prepending the login app and
+# primary navigation apps; apps will all be in the /app directory of the Docker
+# container
 jq -n \
-  --arg apps "$apps" \
-  --arg app_names "$(echo ${app_names[@]})" \
+  --arg app_name "$app_name" \
+  --arg app_file "/app/$packed_app_name.tgz" \
   '{
     "@openmrs/esm-primary-navigation-app": "next",
     "@openmrs/esm-patient-banner-app": "next"
-  } + (
-    ($apps | split("\n")) as $apps |
-    ($app_names | split(" ") | map("/app/" + .)) as $app_files
-    | [$apps, $app_files]
-    | transpose
-    | map({"key": .[0], "value": .[1]})
-    | from_entries
-  )' | jq '{"frontendModules": .}' > "$working_dir/spa-assemble-config.json"
+  } + {
+    ($app_name): $app_file
+  }' | jq '{"frontendModules": .}' > "$working_dir/spa-assemble-config.json"
+echo "Created dynamic spa-assemble-config.json"
 
-echo "Setting up Docker..."
+echo "Copying Docker configuration..."
 cp "$script_dir/Dockerfile" "$working_dir/Dockerfile"
 cp "$script_dir/docker-compose.yml" "$working_dir/docker-compose.yml"
 
 cd "$working_dir"
+echo "Starting Docker containers..."
+# CACHE_BUST to ensure the assemble step is always run
 docker compose build --build-arg CACHE_BUST=$(date +%s) frontend
 docker compose up -d
 
